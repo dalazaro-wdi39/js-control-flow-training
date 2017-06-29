a864e34dd6fa80f6e51c02f85fd9add9599d9876
dalazaro <daryl.lazaro@gmail.com>
2017-06-29 08:47:20 -0700

    completed sing and updated security questions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,17 @@
 console.log("security_questions.js loaded");
+
+// array of security questions
+let securityQuestions = [ "What was your first pet's name?", "What is your mother's maiden name?", "What is your best friend's name?" ];
+
+// array of expected answers
+let expectedAnswers = [ "FlufferNutter", "Remington", "Jimmen Marvinludder" ];
+
+let userInput = [];
+
+// iterate through the array one question at a time
+  for (i = 0; i < securityQuestions.length; i++) {
+      let userAnswer = prompt(securityQuestions[i]);
+      //push userAnswer to the userInput array
+      userAnswer =
+      //if userInput matches expectedAnswer, log in, or else move on to the next prompt
+  }
--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,23 @@
 console.log("sing.js loaded");
+
+// for loop: starts with user input and subtracts 1 on the last line of each iteration
+
+var numBottles = prompt("How many verses would you like to hear?");
+
+for ( i = numBottles; i >= 1; i-- ) {
+  if ( i === 1 ) {
+    console.log (
+    `1 bottle of beer on the wall,
+    1 bottle of beer!
+    Take one down and pass it around,
+    No more bottles of beer on the wall!`
+      )
+  } else {
+    console.log (
+    `${i} bottles of beer on the wall,
+    ${i} bottles of beer!
+    Take one down and pass it around,
+    ${i-1} bottles of beer on the wall!`
+  )
+
+}}
